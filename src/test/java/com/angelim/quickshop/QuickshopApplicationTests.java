0b48cb78e47206ef849197a80b6bfc0b82b2dcc6
angelim <web.angelim@gmail.com>
2026-08-11 20:01:13 -0300

    Initial commit

---
+++ src/test/java/com/angelim/quickshop/QuickshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelim.quickshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
